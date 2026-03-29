7c474d13e55e008db597ea2812bf4442c9331af4
breadpakoda <adityasing272@gmail.com>
2026-03-29 23:11:55 +0530

    problem 74

---
+++ p74.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean searchMatrix(int[][] arr, int target) {
+
+        int row = 0;
+        int col = arr[row].length - 1;
+
+        while (row < arr.length && col >= 0) {
+            if (arr[row][col] == target) {
+                
+                return true;
+            } else if (target > arr[row][col]) {
+                row++;
+            }
+
+            else {
+                col--;
+            }
+        }
+        return false;
+        
+    }
+}
